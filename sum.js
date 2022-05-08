23279609daf9dfb8f565e9077568eaf3396f32c3
jayna922 <leeyijie922@gmail.com>
2022-05-08 14:38:06 +0800

    add sum.js

---
+++ sum.js
@@ -0,0 +1,24 @@
+
+   
+// 回傳 1 + 2 + ... + n 的結果
+function sum(n) {
+    let j=0;
+    for(let i=1; i<=n ; i++){
+        j+=i;
+    }
+  // return 結果
+  return j;
+}
+console.log(sum(1));
+console.log(sum(2)); // 3
+console.log(sum(10)); // 55
+console.log(sum(100)); // 5050
+
+
+
+
+
+
+
+
+

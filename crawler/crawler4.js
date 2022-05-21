2a24dcde01faa977b8c3df7ce37fd75a1dd88e6b
jayna922 <leeyijie922@gmail.com>
2022-05-21 12:16:58 +0800

    up

--- crawler/crawler4.js
+++ crawler/crawler4.js
@@ -9,8 +9,8 @@ const mysql = require('mysql2/promise');
   const connection = await mysql.createConnection({
     host: 'localhost',
     port: 3306,
-    user: 'jayna',
-    password: '123',
+    user: '',
+    password: '',
     database: 'stock',
   });
 
